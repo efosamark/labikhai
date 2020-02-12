11b082475fac43d8daba2e5c5cf0a500b29d1ce7
gr156n <efosamark@gmail.com>
2020-02-12 11:44:52 +0200

    mlml

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, World!");
+QPushButton button("CORONAVIRUS in CHINA!");
 button.resize(200, 60);
 button.show( );
  
