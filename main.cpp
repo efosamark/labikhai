17c6c85a77d60b766e2940240779147e333947e9
gr156n <efosamark@gmail.com>
2020-02-12 12:00:00 +0200

    5tt

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[ ]) 
 {
 QApplication app(argc, argv);
- 
+ kkkkkkkkkkkkkkkkkkkkkkk
 QPushButton button("CORONAVIRUS in CHINA!");
 button.resize(200, 60);
 button.show( );
