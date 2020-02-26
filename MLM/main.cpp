c95de1bfd6e86d0ce73433c5addc5c3cc2cf923b
gr156n <efosamark@gmail.com>
2020-02-26 12:32:46 +0200

    Refactored

--- MLM/main.cpp
+++ MLM/main.cpp
@@ -5,10 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //MainWindow w;
+    MainWindow w;
     QLabel *label = new QLabel ("<h2><i>Hello,</i> " "<font color = red > Qt! </font> </h2>");
     label->show( );
 
-    //w.show();
+    w.show();
     return a.exec();
 }
