96ea9483ee870a86ca6d03af71dd9904d9821349
gr156n <efosamark@gmail.com>
2020-02-26 12:23:34 +0200

    Initial commit

---
+++ MLM/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include <QLabel>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //MainWindow w;
+    QLabel *label = new QLabel ("<h2><i>Hello,</i> " "<font color = red > Qt! </font> </h2>");
+    label->show( );
+
+    //w.show();
+    return a.exec();
+}
